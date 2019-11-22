34b20ac78411e54204c031204164606e706f2e2e
Hang Zhou <zhou_hang@berkeley.edu>
2019-11-21 17:02:37 -0800

    random home

--- algs/input_generator.py
+++ algs/input_generator.py
@@ -7,7 +7,7 @@ def random_name(n):
 	"""
 	n is number of name
 	"""
-	name_set = {"Soda"}
+	name_set = set(["Soda"])
 	while True:
 		rand_name_len = random.randint(0, 10)
 		name = ua[random.randint(0, 25)]
@@ -17,12 +17,28 @@ def random_name(n):
 		if len(name_set) == n:
 			break
 
-	return name_set
+	return list(name_set)
+
+def random_home(name_list, home_num):
+	home_idx_set = set()
+	home_list = []
+	while True:
+		idx = random.randint(0, len(name_list)-1)
+		if idx not in home_idx_set and name_list[idx] is not "Soda":
+			home_idx_set.add(idx)
+			home_list.append(name_list[idx])
+
+		if len(home_list) == home_num:
+			break
+	return home_list
+
 
 
 
 
 if __name__ == '__main__':
 	name_set = random_name(10)
-	for name in name_set:
-		print(name)
+	print(name_set)
+
+	nlist = random_home(name_set, 3)
+	print(nlist)
